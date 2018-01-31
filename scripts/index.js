c1a325c22b00d8023eb46b0bf8e8878a347775b9
Paulo Henrique <paulohenriquepaiva@ymail.com>
2018-01-31 19:05:34 -0200

    Inicialização de Projeto
    Primeiro layout do site FR Planejados

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+function zoomIn(obj)
+{
+    obj.height = obj.height * 3;
+    obj.width = obj.width * 3;
+}
+
+function zoomOut(obj)
+{
+    obj.height = obj.height / 3;
+    obj.width = obj.width / 3;
+}
